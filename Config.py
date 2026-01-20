5974f2f50643211c122b60deb9f828ad14e45a85
Peginti <pegintiiaultra@gmail.com>
2026-01-20 12:39:33 +0100

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+# config.py
+NOM_AGENT = "PEGINTI"
+LANGUE = "fr"
+VERSION = "1.0"
+
+MESSAGES = {
+    "salut": "Bonjour, comment allez-vous ?",
+    "nom": f"Mon nom est {NOM_AGENT}, je suis une intelligence artificielle créée pour vous aider.",
+    "au_revoir": "Au revoir, à bientôt !"
+}
