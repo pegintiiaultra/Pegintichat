271f658551b498960f4e8fef89b4899fa3f638f7
Peginti237 <pegintiiaultra@gmail.com>
2026-02-07 19:47:20 +0100

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,18 @@
+{
+  "name": "pegintichat",
+  "version": "1.0.0",
+  "description": "PEGINTICHAT – MCP core + HTTP API + automated tests.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "jest"
+  },
+  "author": "TOMO OMBEDE BARNABÉ BERTRAND",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "jest": "^29.7.0"
+  }
+}
